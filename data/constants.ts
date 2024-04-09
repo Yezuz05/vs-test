e9df3940eda441c02be88694aaa4e4512c77614f
yezuz05 <temitopeyussuph@gmail.com>
2024-04-09 14:24:33 +0100

    feat: header styles

---
+++ data/constants.ts
@@ -0,0 +1,76 @@
+export const MENU_ITEMS = [
+    {
+        title: 'Courses',
+        link: '#',
+        children: [
+            {
+                title: 'Video courses',
+                link: '#'
+            },
+            {
+                title: 'Vue.js Masterclass',
+                link: '#'
+            },
+            {
+                title: 'Workshops',
+                link: '#'
+            },
+        ]
+    },
+    {
+        title: 'Plans',
+        link: '#'
+    },
+    {
+        title: 'Business Service',
+        link: '#',
+        children: [
+            {
+                title: 'Vue.js Corporate Training',
+                link: '#'
+            },
+            {
+                title: 'Workshops',
+                link: '#'
+            },
+            {
+                title: 'Hiring',
+                link: '#'
+            },
+        ]
+    },
+    {
+        title: 'Learning Paths',
+        link: '#'
+    },
+    {
+        title: 'Hiring',
+        link: '#'
+    },
+    {
+        title: 'Articles',
+        link: '#'
+    },
+    {
+        title: 'Events',
+        link: '#',
+        children: [
+            {
+                title: 'Frontend Nation',
+                link: '#'
+            },
+            {
+                title: 'Vue.js Nation',
+                link: '#'
+            },
+            {
+                title: 'Nuxt Nation',
+                link: '#'
+            },
+        ],
+    },
+    {
+        title: 'Get Certified',
+        link: '#'
+    }
+]
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0BD88F',
+        silver: '#f3f5ff',
+        'phantom-blue': '#212435',
+        'midnight-haze': '#2B2F44'
+      }
+    },
+  },
+  plugins: [],
+}
+
